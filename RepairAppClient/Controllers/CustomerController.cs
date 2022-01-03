5790d6c47ea20c48c603dc7d5a3f74678e11badf
Mattias Selldén <mattias.sellden@gmail.com>
2022-01-03 19:42:09 +0100

    misc changes

---
+++ RepairAppClient/Controllers/CustomerController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RepairAppClient.CaseService;
+
+namespace RepairAppClient.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (CaseServiceClient client = new CaseServiceClient()) 
+            {
+                var customers = client.GetCustomers();
+                if (customers is null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(customers);
+            }
+
+            return View();
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (CaseServiceClient client = new CaseServiceClient())
+            {
+                var customer = client.GetCustomer(id);
+                if (customer is null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(customer);
+            }
+
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
